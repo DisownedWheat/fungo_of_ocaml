99f39ae98c0f59ab0ade9acbf564789311edf971
Robert Larnach <robert.larnach@gmail.com>
2025-06-20 12:10:25 +1000

    wip: add go builtins to typecheck types

--- lib/typecheck/types.ml
+++ lib/typecheck/types.ml
@@ -1,6 +1,34 @@
 open Base
 module Ast = Fungo_ast
 
+type go_builtin =
+  | Bool
+  | String
+  | Int
+  | Int8
+  | Int16
+  | Int32
+  | Int64
+  | U_Int
+  | U_Int8
+  | U_Int16
+  | U_Int32
+  | U_Int64
+  | Byte
+  | Rune
+  | Float32
+  | Float64
+  | Complex64
+  | Complex128
+
+type t =
+  | Record
+  | Module
+
+type base_type =
+  | Go_type
+  | Fg_type of t
+
 type 'a module_def =
   { name : Ast.ASTString.t
   ; types : 'a list
