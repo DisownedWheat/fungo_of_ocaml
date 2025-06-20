5d7ffc7e9935b73bea9dad497b66b2033def78a6
Robert Larnach <robert.larnach@gmail.com>
2025-06-20 11:52:47 +1000

    refactor: change base bin module to use a file instead of raw strings

--- bin/fungo.ml
+++ bin/fungo.ml
@@ -1,18 +1,3 @@
 open Fungo_lib
 
-let input =
-  "\n\
-   \t\t\tmodule X = \n\
-   \t\t\t\ttype t\n\
-   \t\t\t\tlet value x y = [x ; y] \n\
-   \t\t\t\tlet make () = {test = true; test2 = false}\n\
-   \t\t\tend\n\
-   let func a b =\n\
-   \tlet z = X.make () in\n\
-   \tX.value z 1.5\n\n\
-   let () =\n\
-   \tfunc 1 2\n\
-   \t\t\t"
-;;
-
-let () = Frontend.compile input
+let () = Compiler.compile "./test.fg"
